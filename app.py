bdee9abcdc4484b392aa501f4ea6dd11bb5a10ba
“[bridgettenawari]” <[chebetbri@gmail.com]>
2026-07-03 21:46:15 +0300

    done

--- app.py
+++ app.py
@@ -3,23 +3,37 @@ from data import products
 
 app = Flask(__name__)
 
+# Mock Data
+products = [
+    {"id": 1, "name": "Laptop", "price": 899.99, "category": "electronics"},
+    {"id": 2, "name": "Book", "price": 14.99, "category": "books"},
+    {"id": 3, "name": "Desk", "price": 199.99, "category": "furniture"},
+]
+
 # TODO: Implement homepage route that returns a welcome message
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    return {"message": "Welcome to my homepage"}, 200
 
 # TODO: Implement GET /products route that returns all products or filters by category
-
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    category = request.args.get("category")
+    if category:
+        filtered= [product for product in products if product["category"] == category]
+        return jsonify(filtered), 200
+    return jsonify(products), 200
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
-@app.route("/products/<int:id>")
+@app.route("/products/<int:id>", methods=["GET"])
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    for product in products:
+        if product["id"] == id:
+            return jsonify(product), 200
+    return jsonify({"message": "Product not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
